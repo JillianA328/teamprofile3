5769d307c745a0d5687a4250e7b7233c3939f72b
Jillian Amelung <jpamelung@gmail.com>
2021-06-21 22:10:12 -0400

    started index.js

--- teamprofile/index.js
+++ teamprofile/index.js
@@ -11,8 +11,78 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
 
+
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+const promptUser = () => {
+    inquirer.prompt([
+
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Please enter your name:',
+
+        },
+
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Please enter your employee ID:',
+        },
+
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your email address:',
+        },
+
+        {
+            type: 'input',
+            name: 'office',
+            message: 'Please enter your office number:',
+        },
+
+        {
+            type: 'list',
+            choices: ["Manager", "Engineer", "Intern"],
+            message: 'Please select your role:',
+            name: 'role'
+        }
+
+    ]).then(response => {
+        const manager = new Manager9response.managerName, response.managerId, 
+        response.managerEmail, response.officeNumber team.push(manager);
+
+        buildTeam();
+
+    });
+};
+
+// create team
+var teamBuild = function () {
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'Select a team member:'
+            choices: ['Engineer', 'Intern', 'None'],
+            name: 'team',
+        }
+    ])
+    .then(response => {
+        console.log(team);
+        if (response.team === 'Enineer') {
+            engineerInfo();
+        } else if
+        (response.team === 'Intern') {
+            internInfo();
+        } else {
+            fs.createReadStream();
+        }
+        });
+};
+
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
